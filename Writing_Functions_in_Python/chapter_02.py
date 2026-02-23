b7ca3386c0084129b01140722b3b3b6526aa8897
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2026-02-23 20:34:23 +0100

    Add more exercises

---
+++ Writing_Functions_in_Python/chapter_02.py
@@ -0,0 +1,60 @@
+# Open "alice_in_wonderland.txt" and assign the file to "file"
+import contextlib
+from time import time
+
+
+with open('alice_in_wonderland.txt') as file:
+  text = file.read()
+
+n = 0
+for word in text.split():
+  if word.lower() in ['cat', 'cats']:
+    n += 1
+
+print('Lewis Carroll uses the word "cat" {} times'.format(n))
+
+print(type(text))
+print(text[:500])
+
+text_1 = "Hello wonderful world!"
+print(text_1.split())
+
+##########################################
+
+# Add a decorator that will make timer() a context manager
+@contextlib.contextmanager
+def timer():
+  """Time the execution of a context block.
+
+  Yields:
+    None
+  """
+  start = time.time()
+  # Send control back to the context block
+  yield 
+  end = time.time()
+  print('Elapsed: {:.2f}s'.format(end - start))
+
+with timer():
+  print('This should take approximately 0.25 seconds')
+  time.sleep(0.25)
+
+#########################################
+
+@contextlib.contextmanager
+def open_read_only(filename):
+  """Open a file in read-only mode.
+
+  Args:
+    filename (str): The location of the file to read
+
+  Yields:
+    file object
+  """
+# Yield read_only_file so it can be assigned to my_file
+  yield read_only_file
+  # Close read_only_file
+  read_only_file.close()
+
+with open_read_only('my_file.txt') as my_file:
+  print(my_file.read())
