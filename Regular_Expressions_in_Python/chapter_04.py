a84a49716f51dccda4e62a9cb4b41d953730e893
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2026-03-04 12:20:03 +0100

    Add more exercises

--- Regular_Expressions_in_Python/chapter_04.py
+++ Regular_Expressions_in_Python/chapter_04.py
@@ -72,3 +72,92 @@ for tweet in sentiment_analysis:
     
     # Complete format to print out the results
     print("Negative comments found {}".format(negative_matches))
+
+#################################################
+print("#"*50)
+
+contract = "Provider will invoice Client for Services performed within 30 days of performance.  Client will pay Provider as set forth in each Statement of Work within 30 days of receipt and acceptance of such invoice. It is understood that payments to Provider for services rendered shall be made in full as agreed, without any deductions for taxes of any kind whatsoever, in conformity with Provider’s status as an independent contractor. Signed on 03/25/2001."
+
+# Write regex and scan contract to capture the dates described
+regex_dates = r"Signed\son\s(\d{2})/(\d{2})/(\d{4})"
+dates = re.search(regex_dates, contract)
+
+# Assign to each key the corresponding match
+signature = {
+	"day": dates.group(2),
+	"month": dates.group(1),
+	"year": dates.group(3)
+}
+# Complete the format method to print-out
+print("Our first contract is dated back to {data[year]}. Particularly, the day {data[day]} of the month {data[month]}.".format(data=signature))
+
+##################################################
+print("#"*50)
+
+html_tags = [
+    '<body>Welcome to our course! It would be an awesome experience</body>',
+    '<article>To be a data scientist, you need to have knowledge in statistics and mathematics</article>',
+    '<nav>About me Links Contact me!</nav>'
+]
+
+for string in html_tags:
+    # Complete the regex and find if it matches a closed HTML tags
+    match_tag =  re.match(r"<(\w+)>.*?</\1>", string)
+ 
+    if match_tag:
+        # If it matches print the first group capture
+        print("Your tag {} is closed".format(match_tag.group(1))) 
+    else:
+        # If it doesn't match capture only the tag 
+        notmatch_tag = re.match(r"<(\w+)>", string)
+        # Print the first group capture
+        print("Close your {} tag!".format(notmatch_tag.group(1)))
+
+###################################################
+print("#"*50)
+
+sentiment_analysis = [
+    '@marykatherine_q i know! I heard it this morning and wondered the same thing. Moscooooooow is so behind the times',
+    'Staying at a friends house...neighborrrrrrrs are so loud-having a party',
+    'Just woke up an already have read some e-mail'
+]
+
+# Complete the regex to match an elongated word
+regex_elongated = r"\w+(\w)\1\w*"
+
+for tweet in sentiment_analysis:
+	# Find if there is a match in each tweet 
+	match_elongated = re.search(regex_elongated, tweet)
+    
+	if match_elongated:
+		# Assign the captured group zero 
+		elongated_word = match_elongated.group(0)
+        
+		# Complete the format method to print the word
+		print("Elongated word found: {word}".format(word=elongated_word))
+	else:
+		print("No elongated word found") 
+
+###################################################
+print("#"*50)
+
+sentiment_analysis = "You need excellent python skills to be a data scientist. Must be! Excellent python"
+
+# Positive lookahead
+look_ahead = re.findall(r"\w+(?=\spython)", sentiment_analysis)
+
+# Print out
+print(look_ahead)
+
+# Positive lookbehind
+#look_behind = re.findall(r"(?<=python\s|Python\s)\w+", sentiment_analysis)
+look_behind = re.findall(r"(?<=python\s)\w+", sentiment_analysis)
+
+# Print out
+print(look_behind)
+
+##################################################
+print("#"*50)
+
+cellphones = ['4564-646464-01', '345-5785-544245', '6476-579052-01']
+
